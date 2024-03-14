1d1b42741dfb37f30b751aa924f1d48752f5bca8
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-03-14 16:39:50 +0900

    feat: parse succeeded

--- NDLCheck/ContentView.swift
+++ NDLCheck/ContentView.swift
@@ -6,19 +6,55 @@
 //
 
 import SwiftUI
+import SWXMLHash
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: fetchTitleFromNDL, label: {
+                Text("Button")
+            })
         }
         .padding()
+    }
+    
+    private func fetchTitleFromNDL() {
+        Task {
+            do {
+                let isbn = "9784046067234"
+                let url = URL(string: "https://ndlsearch.ndl.go.jp/api/sru?operation=searchRetrieve&query=isbn=\"\(isbn)\"")!
+                let (data, _) = try await URLSession.shared.data(from: url)
+                guard let resultXmlString = String(data: data, encoding: .utf8)
+                else { return }
+                let title = parseXMLStringToFetchBookTitle(xml: resultXmlString)
+                print(title)
+            } catch {
+                print(error)
+            }
+
+        }
+    }
+    
+    private func parseXMLStringToFetchBookTitle(xml outerXmlString: String) -> String {
+        let outerXml = XMLHash.parse(outerXmlString)
+        guard let recordDataElementText = outerXml["searchRetrieveResponse"]["records"]["record"]["recordData"].element?.text
+        else {
+            print("cannot parse")
+            return ""
+        }
+        
+        let recordDataXml = XMLHash.parse(recordDataElementText)
+        guard let title = recordDataXml["srw_dc:dc"]["dc:title"].element?.text
+        else {
+            print("cannot parse")
+            return ""
+        }
+        
+        return title
     }
 }
 
 #Preview {
     ContentView()
 }
+
