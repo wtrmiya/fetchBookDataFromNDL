d0d58ed820d3aeaff4e601d32036bfae0f708f37
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-03-14 12:27:11 +0900

    Initial Commit

---
+++ NDLCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NDLCheck
+//
+//  Created by Wataru Miyakoshi on 2024/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NDLCheck/NDLCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDLCheckApp.swift
+//  NDLCheck
+//
+//  Created by Wataru Miyakoshi on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct NDLCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
